3460034f4502413cb4e538c1f5c2e1194469193a
Akash <akashjio66666@gmail.com>
2020-10-03 23:18:35 +0530

    Update client.js

--- js/client.js
+++ js/client.js
@@ -1,5 +1,5 @@
-const port = process.env.PORT || 8000;
-const socket = io(`http://prox-webapp.herokuapp.com`);
+const PORT = process.env.PORT || 8000;
+const socket = io(`https://prox-webapp.herokuapp.com`);
 
 const form = document.getElementById('send-container');
 const messageInput = document.getElementById('messageInp');
