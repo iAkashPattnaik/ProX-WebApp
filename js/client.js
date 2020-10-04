a6847446b4c8db987bcc5afae869b950e812b841
Akash <akashjio66666@gmail.com>
2020-10-04 14:54:10 +0530

    Add files via upload

--- js/client.js
+++ js/client.js
@@ -1,5 +1,4 @@
-const PORT = process.env.PORT || 8000;
-const socket = io(`https://prox-webapp.herokuapp.com`);
+const socket = io('http://localhost:8000');
 
 const form = document.getElementById('send-container');
 const messageInput = document.getElementById('messageInp');
